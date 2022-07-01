0804cebb8dd9c93fdc574af0f7fd4442de8114fb
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-06-30 22:09:12 -0500

    🐍 Snake Body

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+from turtle import Screen, Turtle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+snake = []
+
+for i in range(0, 3):
+    new_section = Turtle(shape='square')
+    new_section.penup()
+    new_section.color('white')
+    new_section.goto(x=(i*-20), y=0)
+    snake.append(new_section)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+screen.exitonclick()
